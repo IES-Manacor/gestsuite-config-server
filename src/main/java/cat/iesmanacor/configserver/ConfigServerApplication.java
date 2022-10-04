7b4cf1a3bcc7805d8ddd1d0330ef13f72184b0b6
Joan Galmés Riera <joan.galmes@gmail.com>
2022-10-04 20:37:52 +0200

    Init repositori. Migració de multirepo a monorepo

---
+++ src/main/java/cat/iesmanacor/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package cat.iesmanacor.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
